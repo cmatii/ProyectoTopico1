16ecf193f03287133035c47f9abd9ff90a15ddd5
cmatii <matiasvargas@Matiass-MacBook-Pro.local>
2021-03-22 20:59:37 -0300

    Summary

---
+++ STATCOVID/STATCOVID/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using STATCOVID.Models;
+
+namespace STATCOVID.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
